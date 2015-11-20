cc147f298652f413f7055221d5629f993a5a3b47
chxzh <418316606@qq.com>
2015-11-20 18:49:41 -0500

    finished CardGame

--- src/assignment3/CardGame.java
+++ src/assignment3/CardGame.java
@@ -1,5 +1,23 @@
 package assignment3;
 
 public class CardGame {
-
+	public static void main(String[] args) {
+		int playerNum = 4;
+		CardPile dealer = CardPile.makeFullDeck();
+		CardPile[] players = new CardPile[playerNum];
+		for(int i = 0; i < playerNum; i++) {
+			players[i] = new CardPile();
+		}
+		// dealing cards
+		for (int playerToDeal = 0;
+				!dealer.isEmpty();
+				playerToDeal = (playerToDeal+1)%playerNum) {
+			players[playerToDeal].addToBottom(dealer.remove(0));
+		}
+		for(int i = 0; i < playerNum; i++) {
+			if(players[i].find(Suit.SPADES, Value.ACE) != -1){
+				System.out.println("Player "+i+" wins.");
+			}
+		}
+	}
 }
