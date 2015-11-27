1de67d7cd0d1fa88ef8a488138ce1feaac469918
chxzh <418316606@qq.com>
2015-11-27 02:13:50 -0500

    grader constructor done
    and reset(), grade_next(), as well as those supporting private methods

--- src/utils/grade.py
+++ src/utils/grade.py
@@ -7,36 +7,232 @@ try:
 except ImportError:
     from yaml import Loader as Loader
 
+class _Score_point():
+    def __init__(self, subject, action, total,
+                 remark="", deduction=0, deducted=False):
+        self.subject = subject
+        self.action = action
+        self.total = total
+        self.remark = remark
+        self.deduction = deduction
+        self.deducted = deducted
+        
+    def reset(self):
+        self.remark = ""
+        self.deduction = 0
+        self.deducted = False
+        
+    def deduct(self, deduction, remark=""):
+        self.deduction = deduction
+        self.remark = remark
+        self.deducted = True
+    
+    def ask(self):
+        return "(%d pt)Did %s %s?" % \
+                (self.total, self.subject, self.action)
+    
+    def answer(self):
+        response = ""
+        if deducted:
+            response = "(-%d) %s didn't %s." % \
+                    (self.deduction, self.subject, self.action)
+        else:
+            response = "%s did %s." % (self.subject, self.action)
+        if self.remark != "":
+            response += " %s" % (self.remark)
+        return response
+     
+    def brief_answer(self):
+        response = ''
+        if deducted:
+            response = '(-%d) no.' % self.deduction
+        else:
+            response = "yes."            
+        if self.remark != "":
+            response += " %s" % self.remark
+        return response
+
+
+class _Grumble():
+    def __init__(self, deduction, remark):
+        self.deduction = deduction
+        self.remark = remark
+    
+    def ask(self):
+        return 'grumbled that "(-%d) %s"?'% (self.deduction, self.remark)
+    
+    def answer(self):
+        return "(-%d) %s." % (self.deduction, self.remark)
+
+
+class _Subsection():
+    def __init__(self, subject, subsec_dic):
+        self.subject = subject
+        self.total = subsec_dic.pop("total")
+        self.fullmark = self.total
+        try:
+            self.score_points = [_Score_point(self.subject, action, total)\
+                    for action, total in subsec_dic["deduction"].iteritems()]
+        except KeyError:
+            # no deduction specified, add an general one.
+            self.score_points = [_Score_point(self.subject, "do well", self.total)]
+            
+    def reset(self):
+        self.total = self.fullmark
+        for score_point in self.score_points:
+            score_point.reset()
+    
+    def report(self):
+        report = ""
+        for score_point in self.score_points:
+            if score_point.deducted:
+                self.total -= score_point.deduction
+                report += "\n- %s" % (score_point.answer)
+        self.total = max(0, self.total)
+        report = "(%d/%d) %s:%s" % \
+                (self.total, self.fullmark, self.subject, report)
+        return report
+
+
+class _Overall_Section(): # on the same level of _Subsection
+    def __init__(self, sec_title, subsec_dic={"deduction":{}}):            
+        self.subject = sec_title
+        self.deduction = 0
+        self.score_points = [_Score_point(sec_title, action, total)\
+                for action, total in subsec_dic["deduction"].iteritems()]
+        self.extra_remark = []  # [(deduction, remark)]
+    
+    def reset(self):
+        self.deduction = 0
+        self.extra_remark = []
+        for score_point in self.score_points:
+            score_point.reset()
+    
+    def report(self):            
+        report = ""
+        for score_point in self.score_points:
+            if score_point.deducted:
+                self.deduction += score_point.deduction
+                report += "\n- %s" % (score_point.answer)
+        self.total = max(0, self.total)
+        if self.deduction != 0:
+            report = "(-%d) overall:%s" % \
+                    (self.deduction, report)
+        else:
+            report = ""
+        return report
+                
+                
+class _Section():
+    def __init__(self, title, sec_dic):
+        self.title = title
+        self.total = sec_dic.pop("total")
+        self.fullmark = self.total
+        try:
+            self.overall = _Overall_Section(self.title,
+                                       sec_dic.pop("overall"))
+        except KeyError:
+            self.overall = _Overall_Section(self.title)
+        self.subsections = [_Subsection(subject, subsec_dic)\
+                            for subject, subsec_dic in sec_dic.iteritems()]
+    
+    def reset(self):
+        self.total = self.fullmark
+        for subsec in self.subsection:
+            subsec.reset()
+        self.overall.reset()
+    
+    def report(self):
+        report = ""
+        self.total = 0
+        for subsec in self.subsections:
+            report += "\n%s" % subsec.report
+            self.total += subsec.total
+        if self.overall.deduction != 0:
+            report += "\n%s" % self.overall.report()
+            self.total -= self.overall.deduction
+        self.total = max(self.total, 0)
+        report = "---%s (%d/%d)---\n%s" % \
+                (self.title, self.total, self.fullmark, report)
+        return report
+
+
 class Grader:
     def __init__(self, rubric_path):
         with open(rubric_path, 'r') as rubric_file:
-            rubric = yaml.load(rubric_file, Loader)
+            rubric_dic = yaml.load(rubric_file, Loader)
         try:
             # load compulsory components
-            self.fullmark = rubric.pop("_fullmark")
-            self.overall = rubric.pop("_overall")
+            self.fullmark = rubric_dic.pop("fullmark")
+            self.overall = rubric_dic.pop("overall")
         except KeyError as err:
             raise Exception("cannot have root component %s in rubric file %d" % 
                             (err.args[0], rubric_path))
         # load optional components
-        self.signature = self._load_optional(rubric, "_signature", '')
-        self.sections = rubric
+        self.signature = self._load_optional(rubric_dic, 'signature', '')        
+        try:
+            self.overall = _Overall_Section("author", rubric_dic.pop("_overall"))
+        except KeyError:
+            self.overall = _Overall_Section("author")
+        self.sections = [_Section(title, sec_dic) \
+                         for title, sec_dic in rubric_dic.iteritems()]
         self.remark = ""
         self.score = self.fullmark
+        self.records = []
+        self.rewind_stack = []
+        self.grumble_list = []
+        self._score_point_generator = self._generate_score_points()
+        self._next = None
+        self._cur = None
+        self.has_next = False
+        self._get_next_ready()
+    
+    def _get_next_ready(self):
+        try:
+            self._next = self._score_point_generator.next()
+        except StopIteration:
+            self._next = None
+            self.has_next = False
+        else:
+            self.has_next = True
+
+    def _generate_score_points(self):
+        for section in self.sections:
+            for subsection in section.subsections:
+                for score_point in subsection.score_points:
+                    yield score_point
+            for score_point in section.overall:
+                yield score_point
+        for score_point in self.overall:
+            yield score_point            
     
     def _load_optional(self, dic, key, backup):
         try:
             return dic.pop(key)
         except KeyError:
-            return backup
+            return backup    
     
-    def has_next(self):
-        pass
-    
+    def reset(self):
+        self.remark = ""
+        self.score = self.fullmark
+        self.records = []
+        self.rewind_stack = []
+        self.grumble_list = []
+        for section in self.sections:
+            section.reset()
+        self._score_point_generator = self._generate_score_points()
+        self._next = None
+        self._cur = None
+        self.has_next = False
+        self._get_next_ready()
+            
     def grade_next(self):
+        self._cur = self._next
+        self._get_next_ready()
         pass
     
-    def save_current(self):
+    def save_current(self, deduction, remark):
+        self._cur.deduct(deduction, remark)
         pass
     
     def rewind(self):
@@ -57,154 +253,12 @@ class Grader:
     def ask_rewound(self):
         pass
     
-    def reset(self):
-        self.remark = ""
-        self.score = self.fullmark
-        
-    def _wrap_up(self):
-        pass
-    
-    def _rollback(self):
-        pass
-    
     def brief(self):
         pass
     
     def report(self):
         pass
     
-    class _Score_point():
-        def __init__(self, subject, action, total,
-                     remark="", deduction=0, deducted=False):
-            self.subject = subject
-            self.action = action
-            self.total = total
-            self.remark = remark
-            self.deduction = deduction
-            self.deducted = deducted
-            
-        def reset(self):
-            self.remark = ""
-            self.deduction = 0
-            self.deducted = False
-            
-        def deduct(self, deduction, remark=""):
-            self.deduction = deduction
-            self.remark = remark
-            self.deducted = True
-        
-        def ask(self):
-            return "(%d pt)Did %s %s?" % \
-                    (self.total, self.subject, self.action)
-        
-        def answer(self):
-            response = ""
-            if deducted:
-                response = "(-%d) %s didn't %s." % \
-                        (self.deduction, self.subject, self.action)
-            else:
-                response = "%s did %s." % (self.subject, self.action)
-            if self.remark != "":
-                response += " %s" % (self.remark)
-            return response
-         
-        def brief_answer(self):
-            response = ''
-            if deducted:
-                response = '(-%d) no.' % self.deduction
-            else:
-                response = "yes."            
-            if self.remark != "":
-                response += " %s" % self.remark
-            return response
-    
-    
-    class _Subsection():
-        def __init__(self, subject, subsec_dic):
-            self.subject = subject
-            self.total = subsec_dic.pop("_total")
-            self.fullmark = self.total
-            self.score_points = [_Score_point(subject, action, total)\
-                    for action, total in subsec_dic["deduction"].iteritems()]
-        
-        def reset(self):
-            self.total = self.fullmark
-            for score_point in self.score_points:
-                score_point.reset()
-        
-        def report(self):
-            report = ""
-            for score_point in self.score_points:
-                if score_point.deducted:
-                    self.total -= score_point.deduction
-                    report += "\n- %s" % (score_point.answer)
-            self.total = max(0, self.total)
-            report = "(%d/%d) %s:%s" % \
-                    (self.total, self.fullmark, self.subject, report)
-            return report
-    
-    
-    class _Overall_Section(): # on the same level of _Subsection
-        def __init__(self, sec_title, subsec_dic={"deduction":{}}):            
-            self.subject = sec_title
-            self.deduction = 0
-            self.score_points = [_Score_point(sec_title, action, total)\
-                    for action, total in subsec_dic["deduction"].iteritems()]
-            self.extra_remark = []  # [(deduction, remark)]
-        
-        def reset(self):
-            self.deduction = 0
-            self.extra_remark = []
-            for score_point in self.score_points:
-                score_point.reset()
-        
-        def report(self):            
-            report = ""
-            for score_point in self.score_points:
-                if score_point.deducted:
-                    self.deduction += score_point.deduction
-                    report += "\n- %s" % (score_point.answer)
-            self.total = max(0, self.total)
-            if self.deduction != 0:
-                report = "(-%d) overall:%s" % \
-                        (self.deduction, report)
-            else:
-                report = ""
-            return report
-                    
-                    
-    class _Section():
-        def __init__(self, title, sec_dic):
-            self.title = title
-            self.total = sec_dic.pop("_total")
-            self.fullmark = self.total
-            try:
-                self.overall = _Overall_Section(self.title,
-                                           sec_dic.pop("_overall"))
-            except KeyError:
-                self.overall = _Overall_Section(self.title)
-            self.subsections = [_Subsection(subject, subsec_dic)\
-                                for subject, subsec_dic in sec_dic.iteritems()]
-        
-        def reset(self):
-            self.total = self.fullmark
-            for subsec in self.subsection:
-                subsec.reset()
-            self.overall.reset()
-        
-        def report(self):
-            report = ""
-            self.total = 0
-            for subsec in self.subsections:
-                report += "\n%s" % subsec.report
-                self.total += subsec.total
-            if self.overall.deduction != 0:
-                report += "\n%s" % self.overall.report()
-                self.total -= self.overall.deduction
-            self.total = max(self.total, 0)
-            report = "---%s (%d/%d)---\n%s" % \
-                    (self.title, self.total, self.fullmark, report)
-            return report
 
 def _judge_answer(answer):
     answer = answer.lower()
@@ -267,14 +321,15 @@ class _Grade_Cmd:
         return cmd, deduction, remark
     
 def _main():
-    path = _handle_args(sys.argv)
+    #path = _handle_args(sys.argv)
+    path = "../../rubrics/assignment3/assignment3rubric.yaml"
     grader = Grader(path)
     grade_next = True
     counter = 0
     start_time = time.localtime()
     while grade_next:
         subm_grad_time = time.time()
-        while grader.has_next():
+        while grader.has_next:
             settled = False
             grader.grade_next()
             while not settled:
